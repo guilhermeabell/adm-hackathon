b3622c593d78507c65a7fce9ffa5c1270a8de881
FeSilva-dev <feliper.silva011@gmail.com>
2020-09-16 21:37:36 -0300

    new page-login complete

--- src/Pages/private_acess/login/index.tsx
+++ src/Pages/private_acess/login/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import './styles.css'
+import { Link } from 'react-router-dom'
 
 const loginPage = () => {
     return(
-        <div className="loginPage">
-            <h1>Voce está na pagina de login</h1>
-            <p>Seja muito bem vindo</p>
+        <div id="loginPage">
+            <div className="loginPage-content">
+                <div className="loginPage-header">
+                    <p className="header-title">É bom te ver por aqui!</p>
+                </div>
+                <div className="formLogin">
+
+                    <form action="">
+                        <div className="loginForm-field">
+                            <span>Chave de acesso</span>
+                            <input type="text" name="acessKey" id="acessKey" placeholder="Digite sua chave de acesso"/>
+                        </div>
+
+                        <div className="submitButton">
+                            <Link to="/home">
+                                <input type="submit" value="Acessar"/>
+                            </Link>
+                        </div>
+                    </form>
+                </div>
+                <div className="footer-login">
+                    <p className="loginPage-footer">Esta área é permitida apenas para colaboradores.</p>
+                    <p className="loginPage-footer">Faça login para administrar!</p>
+                </div>
+            </div>
         </div>
     )
 }
