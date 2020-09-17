874e0e3fc570c0bbe28cb978713f930e149962d1
Felipe Rodrigues <feliper.silva011@gmail.com>
2020-09-17 03:18:37 -0300

    menu complete

--- src/Pages/private_acess/home/index.tsx
+++ src/Pages/private_acess/home/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './styles.css'
 import icon from './assets/icon.svg'
+import addfile from './assets/addfile.svg'
+import addteam from './assets/addteam.svg'
+import manageprojects from "./assets/manageprojects.svg"
+import showmembers from './assets/showmembers.svg'
+import { Link } from 'react-router-dom'
 
 var data = new Date();
 var hour = data.getHours();
@@ -17,6 +22,45 @@ const loginPage = () => {
                         <p className="user-title" >Olá Pedro,</p>
                         <p className="user-title" >tenha {`${(hour <= 12 ? 'um bom dia!' : hour <= 17 ? 'uma boa tarde!' : 'uma boa noite!')}`}</p>
                     </div>
+                </div>
+
+                <div className="menu-content">
+                    <p>Que tal...</p>
+                </div>
+
+                <div className="shortcuts">
+                    <div className="shortcuts-content">
+                        <div className="itens-menu">
+                            <div className="itens-single">
+                                <Link to="/addfile"> 
+                                    <img src={addfile} alt=""/>
+                                    <p>Adicionar material</p>
+                                </Link>
+                            </div>
+
+                            <div className="itens-single">
+                                <Link to="/addfile"> 
+                                    <img src={addteam} alt=""/>
+                                    <p>Adicionar time</p>
+                                </Link>
+                            </div>
+
+                            <div className="itens-single">
+                                <Link to="/addfile"> 
+                                    <img src={showmembers} alt=""/>
+                                    <p>Ver participantes</p>
+                                </Link>
+                            </div>
+
+                            <div className="itens-single">
+                                <Link to="/addfile"> 
+                                    <img src={manageprojects} alt=""/>
+                                    <p>Gerenciar projetos</p>                                        
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
