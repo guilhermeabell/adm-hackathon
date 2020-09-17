9b47f9d36258f826f7f65f276065e153ed54d0ac
Felipe Rodrigues <feliper.silva011@gmail.com>
2020-09-17 01:26:48 -0300

    start page-login

--- src/Pages/private_acess/home/index.tsx
+++ src/Pages/private_acess/home/index.tsx
@@ -2,11 +2,23 @@ import React from 'react'
 import './styles.css'
 import icon from './assets/icon.svg'
 
+var data = new Date();
+var hour = data.getHours();
+
 const loginPage = () => {
     return(
         <div className="homeHeader">
-            <p>Oi</p>
-            <img src={icon} alt=""/>
+            <div className="home-content">
+                <div className="home-header">
+                    <div className="user-section-img">
+                        <img src={icon}></img>
+                    </div>
+                    <div className="user-section-title">
+                        <p className="user-title" >Olá Pedro,</p>
+                        <p className="user-title" >tenha {`${(hour <= 12 ? 'um bom dia!' : hour <= 17 ? 'uma boa tarde!' : 'uma boa noite!')}`}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
